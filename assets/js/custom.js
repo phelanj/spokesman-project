4eed94c4acc2d79bb473d3dfb4285188ff88dca5
Jack Phelan <phelanj@users.noreply.github.com>
2020-03-29 18:46:31 -0700

    upload project

---
+++ assets/js/custom.js
@@ -0,0 +1,56 @@
+/* ----------------------------
+/*  Name: spokesman-project
+    Author: Jack Phelan
+    Version: 0
+/* -------------------------- */
+var slideArr = ["jesse","meehan","don","emma","azaria","amy","adam","shawn","arielle","jim","molly","rob"];
+var buttons = document.getElementsByClassName("imgIcon");
+var slides = document.getElementsByClassName("slide");
+var cycle = 7000;
+var timeout;
+
+//gets clicked element id
+//calls updateSlide
+//call recursive fn slideshow
+function clickImage() {
+    var buttonId = event.srcElement.id;
+    updateSlide(buttonId);
+        var foo = slideArr.indexOf(buttonId);
+        clearTimeout(timeout);
+        slideshow(foo, buttons.length);
+}
+//removes all 'selected' classes (needs to save one)
+//removes all displayed slides except selected slide
+//calls slideshow
+function updateSlide(buttonId) {
+    var slideId;
+    for (i = 0; i < buttons.length; i++) {
+        if (buttons[i].classList.contains("selected")) { 
+        buttons[i].classList.remove("selected");
+        slideId = slides[i].id;
+        document.getElementById(slideId).classList.remove("showSlide");
+        //document.getElementById(slideId).classList.add("hideSlide");
+    }
+    }
+    //document.getElementById(buttonId + "_slide").classList.remove("hideSlide");
+    document.getElementById(buttonId + "_slide").classList.add("showSlide");
+    document.getElementById(buttonId).classList.add("selected");
+}
+
+//hide previous slide and display current slide, recursive function
+function slideshow(i, length) {
+    for (j = 0; j < length; j++) {
+    slides[j].classList.remove("showSlide");
+    //slides[j].classList.add("hideSlide");
+    buttons[j].classList.remove("selected");
+    }
+    if (i === 12) {i = 0}
+    buttons[i].classList.add("selected");
+    //slides[i].classList.remove("hideSlide");
+    slides[i].classList.add("showSlide");
+    timeout =  setTimeout(slideshow, cycle, ++i, length);  
+}
+
+
+
+setTimeout(slideshow, cycle, 1, buttons.length);
